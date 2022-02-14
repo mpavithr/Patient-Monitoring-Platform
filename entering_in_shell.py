64f23410965dc928da89f397f392bd706e39d327
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-02-13 21:25:27 -0500

    Create entering_in_shell.py
    Shell for device interface

---
+++ entering_in_shell.py
@@ -0,0 +1,12 @@
+import string
+import numpy as np
+import os
+
+def shell():
+  #Creates an interface for users to add in their data
+
+def main():
+    shell()
+
+if __name__=="__main__":
+    main()
