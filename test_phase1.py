3635a194937dfefa4c13be24e9446bc98af1d161
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-02-25 21:17:19 -0500

    Update test_phase1.py

--- test_phase1.py
+++ test_phase1.py
@@ -1,12 +1,12 @@
 import unittest
-import phase1
+import test
 
 class TestPhase1(unittest.TestCase):
     def test_checkingForJSON(self):
-        result = phase1.checkingForJSON("devices.json")
+        result = test.checkingForJSON("devices.json")
         self.assertEqual(result,1)
     def test_checkingEmptyFile(self):
-        result = phase1.checkingEmptyFile("devices.json")
+        result = test.checkingEmptyFile("devices.json")
         self.assertEqual(result,1)
 
         
