af73f66e92785ca6890a92f85666bba33c8a8caf
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-03-13 16:17:08 -0400

    Update test_device_module.py

--- test_device_module.py
+++ test_device_module.py
@@ -3,31 +3,31 @@ import device_module_unit_tests
 
 class TestPhase1(unittest.TestCase):
     def test_checkingForJSON(self):
-        result = device_module_unit_tests.checkingForJSON("devices.txt")
+        result = device_module_unit_tests.checkingForJSON("/data/devices.txt")
         self.assertEqual(result,0)
     def test_checkingEmptyEntryforFile(self):
         result = device_module_unit_tests.checkingEmptyEntryforFile(None)
         self.assertEqual(result,0)
     def test_checkingEmptyFile(self):
-        result = device_module_unit_tests.checkingEmptyFile("empty_json.json")
+        result = device_module_unit_tests.checkingEmptyFile("/data/empty_json.json")
         self.assertTrue(result,True)
     def test_correct_keys_for_device_json(self):
-        result = device_module_unit_tests.correct_keys_for_device_json("incorrect_keys_devices.json")
+        result = device_module_unit_tests.correct_keys_for_device_json("/data/incorrect_keys_devices.json")
         self.assertEqual(result,0)
     def test_incorrect_no_of_keys_device_json(self):
-        result = device_module_unit_tests.correct_keys_for_device_json("incorrect_num_keys_device.json")
+        result = device_module_unit_tests.correct_keys_for_device_json("/data/incorrect_num_keys_device.json")
         self.assertEqual(result,0)
     def test_correct_keys_for_measurement_json(self):
-        result = device_module_unit_tests.correct_keys_for_measurement_json("devices.json")
+        result = device_module_unit_tests.correct_keys_for_measurement_json("/data/devices.json")
         self.assertEqual(result,0)
     def test_incorrect_no_of_keys_measurement_json(self):
-        result = device_module_unit_tests.correct_keys_for_measurement_json("devices.json")
+        result = device_module_unit_tests.correct_keys_for_measurement_json("/data/devices.json")
         self.assertEqual(result,0)
     def test_correct_value_for_device_json(self):
-        result = device_module_unit_tests.correct_value_for_device_json("incorrect_values_devices.json")
+        result = device_module_unit_tests.correct_value_for_device_json("/data/incorrect_values_devices.json")
         self.assertEqual(result,0)
     def test_correct_value_for_measurement_json(self):
-        result = device_module_unit_tests.correct_value_for_measurement_json("incorrect_values_measurements.json")
+        result = device_module_unit_tests.correct_value_for_measurement_json("/data/incorrect_values_measurements.json")
         self.assertEqual(result,0)
 
 
