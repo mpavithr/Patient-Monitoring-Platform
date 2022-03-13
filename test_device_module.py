9fb89f0bbc9eef8ea6aaa5ce4406f033d0e19182
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-03-13 16:11:15 -0400

    Update and rename test_deviceRESTAPI.py to test_device_module.py

---
+++ test_device_module.py
@@ -0,0 +1,35 @@
+import unittest
+import device_module_unit_tests
+
+class TestPhase1(unittest.TestCase):
+    def test_checkingForJSON(self):
+        result = device_module_unit_tests.checkingForJSON("devices.txt")
+        self.assertEqual(result,0)
+    def test_checkingEmptyEntryforFile(self):
+        result = device_module_unit_tests.checkingEmptyEntryforFile(None)
+        self.assertEqual(result,0)
+    def test_checkingEmptyFile(self):
+        result = device_module_unit_tests.checkingEmptyFile("empty_json.json")
+        self.assertTrue(result,True)
+    def test_correct_keys_for_device_json(self):
+        result = device_module_unit_tests.correct_keys_for_device_json("incorrect_keys_devices.json")
+        self.assertEqual(result,0)
+    def test_incorrect_no_of_keys_device_json(self):
+        result = device_module_unit_tests.correct_keys_for_device_json("incorrect_num_keys_device.json")
+        self.assertEqual(result,0)
+    def test_correct_keys_for_measurement_json(self):
+        result = device_module_unit_tests.correct_keys_for_measurement_json("devices.json")
+        self.assertEqual(result,0)
+    def test_incorrect_no_of_keys_measurement_json(self):
+        result = device_module_unit_tests.correct_keys_for_measurement_json("devices.json")
+        self.assertEqual(result,0)
+    def test_correct_value_for_device_json(self):
+        result = device_module_unit_tests.correct_value_for_device_json("incorrect_values_devices.json")
+        self.assertEqual(result,0)
+    def test_correct_value_for_measurement_json(self):
+        result = device_module_unit_tests.correct_value_for_measurement_json("incorrect_values_measurements.json")
+        self.assertEqual(result,0)
+
+
+if __name__ == '__main__':
+    unittest.main()
